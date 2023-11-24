a1f4646c58f071914d28a56d213b13da69c2e13f
David Darx <david.darx@liip.ch>
2023-11-24 12:59:45 +0100

    feat(active-link): update switch to page router version

--- src/components/core/ActiveLink.tsx
+++ src/components/core/ActiveLink.tsx
@@ -1,31 +1,42 @@
-'use client';
-
+/* based on https://github.com/vercel/next.js/blob/canary/examples/active-class-name/components/ActiveLink.tsx */
 import Link, { LinkProps } from 'next/link';
-import { usePathname } from 'next/navigation';
-import { PropsWithChildren } from 'react';
+import { useRouter } from 'next/router';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 
 type ActiveLinkProps = LinkProps & {
-  className: string;
-  activeClassName: string;
+  className?: string;
+  activeClassName?: string;
 };
 
-export default function NavLink({
+const ActiveLink = ({
   children,
-  className = '',
   activeClassName = '',
+  className = '',
   ...props
-}: PropsWithChildren<ActiveLinkProps>) {
-  const currentRoute = usePathname();
+}: PropsWithChildren<ActiveLinkProps>) => {
+  const { asPath, isReady } = useRouter();
+  const [computedClassName, setComputedClassName] = useState(className);
 
-  let computedClass = className;
+  useEffect(() => {
+    if (isReady) {
+      const linkPathname = new URL((props.as || props.href) as string, location.href).pathname;
 
-  if (props.href === currentRoute) {
-    computedClass += ` ${activeClassName}`;
-  }
+      const activePathname = new URL(asPath, location.href).pathname;
+
+      const newClassName =
+        linkPathname === activePathname ? `${className} ${activeClassName}`.trim() : className;
+
+      if (newClassName !== computedClassName) {
+        setComputedClassName(newClassName);
+      }
+    }
+  }, [asPath, isReady, props.as, props.href, activeClassName, className, computedClassName]);
 
   return (
-    <Link scroll={false} className={computedClass} {...props}>
+    <Link scroll={false} className={computedClassName} {...props}>
       {children}
     </Link>
   );
-}
+};
+
+export default ActiveLink;
