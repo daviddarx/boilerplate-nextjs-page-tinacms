5d3c7c5775b51f2dde272db1830c515c8758f6a7
David Darx <david.darx@liip.ch>
2024-03-03 16:39:47 +0100

    feat(layout): add font

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,10 @@
 import Header from '@/components/layout/Header';
 import Navigation from '@/components/layout/Navigation';
 import { NavigationResult } from '@/types';
+import classNames from 'classnames';
+import { Bricolage_Grotesque } from 'next/font/google';
+
+const font = Bricolage_Grotesque({ weight: ['400', '600'], subsets: ['latin'] });
 
 export default function Layout({
   navigationProps,
@@ -10,7 +14,7 @@ export default function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='lg:grid lg:grid-cols-2'>
+    <div className={classNames('lg:grid lg:grid-cols-2', font.className)}>
       <Header />
       {navigationProps && <Navigation {...navigationProps} />}
       <main className='p-gutter lg:px-0 lg:pr-gutter'>{children}</main>
--- src/components/layout/PageWrapper.tsx
+++ src/components/layout/PageWrapper.tsx
@@ -1,13 +1,10 @@
 import Footer from '@/components/layout/Footer';
-import { Inter } from 'next/font/google';
 import React, { ReactNode } from 'react';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <React.Fragment>
-      <main>{children}</main>
+      {children}
       <Footer />
     </React.Fragment>
   );
