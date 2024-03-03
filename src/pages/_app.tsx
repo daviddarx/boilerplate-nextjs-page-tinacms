20a9251fdf5af946df0fb9381c5d597864d1bd80
David Darx <david.darx@liip.ch>
2024-03-03 16:39:01 +0100

    feat(store): implement store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
 import Layout from '@/components/layout/Layout';
+import store from '@/store/';
+import { uiActions } from '@/store/';
 import '@/styles/globals.css';
+import { getSystem } from '@/utils/core';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const system = getSystem();
+
+    document.body.classList.add(system.os);
+    document.body.classList.add(system.browser);
+
+    store.dispatch(
+      uiActions.setSystem({
+        os: system.os,
+        browser: system.browser,
+      }),
+    );
+  });
+
   return (
-    <Layout {...pageProps}>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
